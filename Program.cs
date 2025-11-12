c44e437e18c7a4911986a3dc5e799ff22ad70026
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-11-12 15:43:00 +0200

    feat(app): deserialize books.json and print; write base serialization

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using OopJsonLab.Models;
+
+var path = Path.Combine(AppContext.BaseDirectory, "Data", "books.json");
+
+var options = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true,
+    WriteIndented = true
+};
+
+List<Book>? books;
+await using (var fs = File.OpenRead(path))
+{
+    books = await JsonSerializer.DeserializeAsync<List<Book>>(fs, options);
+}
+
+if (books is null)
+{
+    Console.WriteLine("Не вдалося прочитати дані.");
+    return;
+}
+
+foreach (var b in books)
+{
+    Console.WriteLine($"[{b.PublishingHouseId}] \"{b.Title}\" / {b.PublishingHouse?.Name} ({b.PublishingHouse?.Address})");
+}
+
+var outPath = Path.Combine(AppContext.BaseDirectory, "out_base.json");
+await File.WriteAllTextAsync(outPath, JsonSerializer.Serialize(books, options));
+Console.WriteLine($"\nБазову сереалізацію збережено у: {outPath}");
